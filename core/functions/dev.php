0fdb8dca9ed374b3a98cbf44054b3670288a7f24
Chris Galbraith <chris@resknow.co.uk>
2020-05-08 21:56:42 +0100

    Add JSON helper function

--- core/functions/dev.php
+++ core/functions/dev.php
@@ -12,3 +12,19 @@ function dump( $data ) {
     $string = print_r($data, true);
     printf('<pre>%s</pre>', $string);
 }
+
+/**
+ * JSON
+ * 
+ * Return some data as JSON and set the response code and
+ * content type header
+ * 
+ * @param array|object $data
+ * @param int $response_code
+ * @return string JSON encoded string
+ */
+function json($data, $response_code = 200) {
+    header('Content-type: application/json');
+    http_response_code($response_code);
+    return json_encode($data);
+}
